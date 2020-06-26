05677589fb8d1858da103c6ea900d2bc66a34b3c
mvermadev <manishverma001997@gmail.com>
2020-06-26 21:08:37 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         The RecruitingMonk Development in MV branch.
+         The RecruitingMonk Development in MV branch load-2.
         </p>
         <a
           className="App-link"
