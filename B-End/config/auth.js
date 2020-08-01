f7e3512c9b0d97545bf9dee70d87bd1a62965017
prakash-bidgely <prakash@bidgely.com>
2020-08-02 03:21:03 +0530

    Added routes for jwt

---
+++ B-End/config/auth.js
@@ -0,0 +1,29 @@
+module.exports = {
+    'googleAuth': {
+        'clientID': '468532707571-tv95cvhjfvoek81nfj2ls9f9ke9c1gi6.apps.googleusercontent.com',
+        'clientSecret': 'zfIzBVqKC0FWA9XVNFQ8ko1I',
+        'callbackURL': 'https://vigilant-mayer-030069.netlify.app/auth/google/callback'
+    }
+};
+
+
+module.exports = {
+    'googleAuth': {
+        'clientID': '468532707571-tv95cvhjfvoek81nfj2ls9f9ke9c1gi6.apps.googleusercontent.com',
+        'clientSecret': 'zfIzBVqKC0FWA9XVNFQ8ko1I',
+        'callbackURL': 'https://vigilant-mayer-030069.netlify.app/auth/google/callback'
+    },
+    "facebook":{
+        "app_id":"2231397523760486",
+        "app_secret":"c97da2d05746f519c3e939972c6eb30a",
+        "callback":"https://vigilant-mayer-030069.netlify.app/auth/facebook/callback"
+    },
+    "linkedin":{
+        'clientID': '817qcs4o6awr3u',
+        'clientSecret': 'pzaBFyEGEvCmtQLO',
+        'callbackURL': 'http://localhost:3000/auth/linkedin/callback'
+    },
+    'sendgrid': {
+        'apiKey': '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'
+    }
+};
---
+++ B-End/routes/users.js
@@ -0,0 +1,12 @@
+var express = require('express');
+const passport = require("passport");
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
+
+module.exports = router;
