597bddf9a14c130ddaf8b7eb75b9315028a2de90
JT-77 <jatin7599@gmail.com>
2020-08-06 11:26:15 +0530

    importing

--- F-End/src/components/Pages/Community/Community.js
+++ F-End/src/components/Pages/Community/Community.js
@@ -4,7 +4,6 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
-import '../Page.css'
 
 function Monks() {
     return(
