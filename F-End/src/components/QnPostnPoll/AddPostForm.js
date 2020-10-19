0206a3dbffcf428cc239fd38040dab9b75699d52
JT-77 <jatin7599@gmail.com>
2020-10-19 11:33:10 +0530

    lib int.

--- F-End/src/components/QnPostnPoll/AddPostForm.js
+++ F-End/src/components/QnPostnPoll/AddPostForm.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Grid, InputBase, Typography, InputLabel, NativeSelect, Button } from '@material-ui/core';
-import CameraAltIcon from '@material-ui/icons/CameraAlt';
+import AttachmentRoundedIcon from '@material-ui/icons/AttachmentRounded';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import IconButton from '@material-ui/core/IconButton';
+import Swal from 'sweetalert2'
 
 function AddPost(props) {
 
     const [state, setState] = React.useState({
         uploaded_by: localStorage.id,
-        video: '',
+        video: 'empty',
         category: '',
         sub_category: '',
         content: '',
         title: ''
     });
+
+    const [file, setFile] = React.useState(null);
 
     const handleInput = (event) => {
         setState({
@@ -22,26 +25,51 @@ function AddPost(props) {
         })
     };
 
+    const handleFileInput = (event) => {
+        setFile(event.target.files[0])
+    };
+
+    const handleCancel = () => {
+        window.location.href = "/"
+    };
+
     const handleSubmit = (e) => {
         e.preventDefault();
-
-        var myHeaders = new Headers();
-        myHeaders.append("Authorization", localStorage.token);
-        myHeaders.append("Content-Type", "application/json");
-
-        var raw = JSON.stringify(state);
+        console.log(state)
+        console.log(file)
+        var formdata = new FormData();
+        if(file != null)
+            formdata.append("file", file);
+        formdata.append("content", JSON.stringify(state));
 
         var requestOptions = {
             method: 'POST',
-            headers: myHeaders,
-            body: state,
+            body: formdata,
             redirect: 'follow'
         };
 
         fetch("https://recmonk.herokuapp.com/library", requestOptions)
-            .then(response => response.json())
+            .then(response => {
+                if(response.ok) {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Post Added!'
+                      })
+                    return response;
+                }
+            })
             .then(result => console.log(result))
             .catch(error => console.log('error', error));
+        
+        setFile(null)
+        setState({
+            uploaded_by: localStorage.id,
+            video: 'empty',
+            category: '',
+            sub_category: '',
+            content: '',
+            title: ''
+        })
     }
 
     return (
@@ -81,33 +109,33 @@ function AddPost(props) {
                         <Typography>Select Post Category</Typography>
                     </Grid>
                     <Grid xs={6} sm={5} md={3} className="cat-chooser">
-                    <InputLabel htmlFor="sub_category" />
-                    <NativeSelect id="sub_category" name="sub_category" className="cat-chooser-select" required={true} fullWidth
-                        value={state.sub_category}  onChange={handleInput} >
-                        <option value="" disabled selected>Category</option>
-                        <option value="Career">Career</option>
-                        <option value="Engagement">Engagement</option>
-                        <optgroup label="HRTech">
-                            <option value="Automation">Automation</option>
-                            <option value="HR Analytics">HR Analytics</option>
-                            <option value="HRMS/ATS">HRMS/ATS</option>
-                        </optgroup>
+                        <InputLabel htmlFor="sub_category" />
+                        <NativeSelect id="sub_category" name="sub_category" className="cat-chooser-select" required={true} fullWidth
+                            value={state.sub_category} onChange={handleInput} >
+                            <option value="" disabled selected>Category</option>
+                            <option value="Career">Career</option>
+                            <option value="Engagement">Engagement</option>
+                            <optgroup label="HRTech">
+                                <option value="Automation">Automation</option>
+                                <option value="HR Analytics">HR Analytics</option>
+                                <option value="HRMS/ATS">HRMS/ATS</option>
+                            </optgroup>
                             <option value="Interviewing">Interviewing</option>
                             <option value="Job Req">Job Req</option>
-                        <optgroup label="Marketing">
-                            <option value="Branding">Branding</option>
-                            <option value="Mail/SMS">Mail/SMS</option>
-                            <option value="Story Telling">Story Telling</option>
-                        </optgroup>
+                            <optgroup label="Marketing">
+                                <option value="Branding">Branding</option>
+                                <option value="Mail/SMS">Mail/SMS</option>
+                                <option value="Story Telling">Story Telling</option>
+                            </optgroup>
                             <option value="Others">Others</option>
-                        <optgroup label="Recruiting">
-                            <option value="Non-IT Hiring">Non-IT Hiring</option>
-                            <option value="Tech Hiring">Tech Hiring</option>
-                        </optgroup>
+                            <optgroup label="Recruiting">
+                                <option value="Non-IT Hiring">Non-IT Hiring</option>
+                                <option value="Tech Hiring">Tech Hiring</option>
+                            </optgroup>
                             <option value="Screening">Screening</option>
                             <option value="Sourcing">Sourcing</option>
                             <option value="Startup">Startup</option>
-                    </NativeSelect>
+                        </NativeSelect>
                     </Grid>
                 </Grid>
 
@@ -118,15 +146,14 @@ function AddPost(props) {
                         fullWidth
                         rowsMax={20}
                         rows={5}
-                        required
                         name="content"
                         value={state.content} onChange={handleInput}
                         endAdornment={
                             <InputAdornment position="end">
-                                <input accept="file/*" id="post-attachment-file" type="file" />
+                                <input style={{ display: 'none' }} name="file" id="post-attachment-file" onChange={handleFileInput} type="file" />
                                 <label htmlFor="post-attachment-file">
-                                    <IconButton className="post-file-attach" aria-label="attachment" component="span">
-                                        <CameraAltIcon />
+                                    <IconButton className="post-file-attach" aria-label="attachment" component="p">
+                                        {file!= null ? <p>{file.name}</p> : <p></p>} <AttachmentRoundedIcon />
                                     </IconButton>
                                 </label>
                             </InputAdornment>
@@ -138,7 +165,7 @@ function AddPost(props) {
                     <br />
                 </Grid>
                 <Grid item xs={12} align="right">
-                    <Button type="cancel" style={{ background: '#fff', marginRight: '10px' }}>Cancel</Button>
+                    <Button onClick={handleCancel} style={{ background: '#fff', marginRight: '10px' }}>Cancel</Button>
                     <Button type="submit" style={{ background: '#b32800', color: '#fff', marginRight: '10px' }}>Add</Button>
                 </Grid>
             </Grid>
